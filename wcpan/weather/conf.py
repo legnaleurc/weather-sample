a2c090a8446cb409bfd0c789bd309e3953c32e54
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-06-01 12:17:53 +0900

    extract variable

---
+++ wcpan/weather/conf.py
@@ -0,0 +1,12 @@
+import os.path as op
+
+
+def get_db_dsn():
+    tmp = op.dirname(__file__)
+    tmp = op.join(tmp, '../..')
+    tmp = op.normpath(tmp)
+    tmp = op.join(tmp, 'db.sqlite')
+    return tmp
+
+
+DATABASE_DSN = get_db_dsn()
